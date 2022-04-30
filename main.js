110cf00120e27d3764e6fe6ff2e84414b73e7298
Charles <charlescon@live.com>
2022-04-30 16:32:41 -0500

    reset button working

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-
+    const initial = document.getElementById('start').innerHTML;
+    const reset = document.querySelector('.reset');
+    let html;
+    window.onload = function() {
+        html = document.querySelector('#start').innerHTML;
+    }
+    reset.onclick = () => {
+        document.getElementById('start').innerHTML = html;
+    }
 });
